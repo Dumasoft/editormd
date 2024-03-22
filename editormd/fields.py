4e45c04a9ba3a3e8b140b0b540b4b2b75b24ed10
bronigege <bronigege@gmail.com>
2024-03-22 09:23:07 +0100

    Funcionalidad básica

---
+++ editormd/fields.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.db import models
+
+from ..editormd.widgets import MDWidget
+
+
+class MDFormField(forms.fields.CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs.update({
+            'widget': MDWidget(),
+        })
+
+        super(MDFormField, self).__init__(*args, **kwargs)
+
+
+class MDField(models.TextField):
+    def __init__(self, *args, **kwargs):
+        super(MDField, self).__init__(*args, **kwargs)
+
+    def formfield(self, **kwargs):
+        defaults = {
+            'form_class': MDFormField,
+        }
+        defaults.update(kwargs)
+        return super(MDField, self).formfield(**defaults)
